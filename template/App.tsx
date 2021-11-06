13a0451a6cdd9095a4332a2ebb6248eb2412ce6f
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-11-06 12:22:34 +0530

    :tada: Created template

---
+++ template/App.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { Appearance } from "react-native";
+import { connect } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AppNavigator from "./navigation/AppNavigator";
+import AuthNavigator from "./navigation/AuthNavigator";
+import { ChangeMode } from "./store/theme/actions";
+
+function App(props: any) {
+  // State Variables
+  const { User, Theme } = props;
+
+  // Dispatch Functions
+  const { ChangeMode } = props;
+
+  // Theme Change Listener
+  useEffect(() => {
+    const subscribe = Appearance.addChangeListener(onThemeChange);
+
+    return () => subscribe.remove();
+  }, []);
+
+  // Function to execute on device theme change
+  const onThemeChange = ({ colorScheme }: any) => {
+    // Change your theme here
+    console.log(colorScheme);
+    ChangeMode(colorScheme);
+  };
+
+  return (
+    <NavigationContainer theme={Theme}>
+      {User ? <AppNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+}
+
+const mapStateToProps = ({ Auth, Theme }: any) => {
+  return {
+    User: Auth.User,
+    Theme: Theme.Theme,
+  };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    ChangeMode: (colorScheme: any) => dispatch(ChangeMode(colorScheme)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
